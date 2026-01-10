a4f9f053c4b12454ee95507fc1e7dd965ce335c6
Aakashkalla <aakashpradeepkalla@gmail.com>
2026-01-10 16:02:47 +0530

    MongoDB Added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
+import dotenv from 'dotenv'
+dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (req,res) => {
     res.json({
-        message : "Server Started"
+        message : "Hello From Get"
     })
 })
 
-app.listen(3000, ()=>{
-    console.log("Server Started")
-})
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGODB_URL!);
+        console.log("DB Connected");
+    }catch(e){
+        console.log(e);
+        throw new Error;
+    }
+}
+
+async function serverStart(){
+    try{
+        await connectDB()
+        app.listen(3000, async ()=>{
+            console.log("Server Started");
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+
+serverStart()
