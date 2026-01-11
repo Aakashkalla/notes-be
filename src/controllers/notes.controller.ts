1941929af1724f9790974d7b78e7aadf174e16f6
Aakashkalla <aakashpradeepkalla@gmail.com>
2026-01-11 21:24:36 +0530

    Delete Note Added

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -123,3 +123,35 @@ export async function updateNote(req:Request, res : Response){
     }
     
 }
+
+export async function deleteNote(req : Request, res : Response){
+    const userId = req.userId;
+    if(!userId){
+        return res.status(401).json({
+            message : "Unauthorized"
+        });
+    }
+    const noteId = req.params.noteId;
+
+    try{
+        const deletedNote = await NoteModel.findOneAndDelete({
+            _id : noteId, userId
+        });
+
+        if(!deletedNote){
+            return res.status(404).json({
+                message : "Note not Found"
+            })
+        }else{
+            return res.status(200).json({
+                message : "Note Deleted"
+            })
+        }
+        
+    }catch(e){
+        res.status(500).json({
+            message : "Server Error!"
+        });
+        console.log(e);
+    }
+}
