c060a0c7e07a85af03ad5dc01dd25f4430789dca
Aakashkalla <aakashpradeepkalla@gmail.com>
2026-01-11 20:59:09 +0530

    Added Update Note

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -69,3 +69,57 @@ export async function getNotes(req:Request, res:Response){
         console.log(e)
     }
 }
+
+export async function updateNote(req:Request, res : Response){
+    const requiredBody = z.object({
+        title : z.string().nonempty(),
+        content : z.string().nonempty()
+    })
+    
+    const parsedBody = requiredBody.safeParse(req.body);
+
+    if(!parsedBody.success){
+        res.status(400).json({
+            message : "Invalid Input",
+            errors : parsedBody.error.issues
+        });
+        return;
+    }
+
+    const {title, content} = parsedBody.data;
+    const userId = req.userId;
+    const noteId = req.params.noteId
+    if(!userId){
+        return res.status(401).json({
+            message : "Unauthorized"
+        })
+    }
+
+    try{
+        const updatedNote = await NoteModel.findOneAndUpdate({
+            _id: noteId, userId
+        },
+        {
+            title,
+            content
+        },
+        {new : true}
+        )
+        if(updatedNote){
+            res.status(200).json({
+                message: "Updated Note",
+                updatedNote
+            })
+        }else{
+            res.status(404).json({
+                message : "Note Not Found"
+            })
+        }
+    }catch(e){
+        res.status(500).json({
+            message : "Server Error!"
+        });
+        console.log(e)
+    }
+    
+}
