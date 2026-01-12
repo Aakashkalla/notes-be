1fb0b077ad85f3e48d840c7b894aa88e0c1cbd68
Aakashkalla <aakashpradeepkalla@gmail.com>
2026-01-12 14:48:19 +0530

    Added CORS

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@ import { connectDB } from './db.js';
 import cookieParser from "cookie-parser";
 import { authRouter } from "./routes/auth.routes.js";
 import { noteRouter } from "./routes/notes.routes.js";
+import cors from 'cors';
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
